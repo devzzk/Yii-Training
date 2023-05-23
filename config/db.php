a8a48780e37d9105e549e701a873e4a30baf1783
Donny <qq1507438135@163.com>
2023-05-23 14:34:32 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql:3306;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8mb4',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
